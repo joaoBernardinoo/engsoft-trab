baaa781a94dbf8f80d2ee1f4595435febe3531c9
joaoBernardinoo <joaobernardino@ufba.br>
2025-02-01 20:50:20 -0300

    refatoração no código

--- strategy/aluno_pos_graduacao.py
+++ strategy/aluno_pos_graduacao.py
@@ -2,9 +2,18 @@ from strategy.emprestimo import EmprestimoStrategy
 
 class EmprestimoAlunoPosGraduacaoStrategy(EmprestimoStrategy):
     def pode_emprestar(self, user, book):
-        # Implementar regras específicas para alunos de pós-graduação
-        # ...existing code...
-        pass
+        # Regra de Empréstimo para Alunos de Pós Graduação
+        if not book.exemplares_disponiveis():
+            return False
+        if user.is_devedor():
+            return False
+        if user.livros_emprestados_count() >= 3:
+            return False
+        if book.reservas_count() >= book.exemplares_count() and not book.is_reserved_by(user):
+            return False
+        if user.has_emprestimo(book):
+            return False
+        return True
 
     def tempo_emprestimo(self):
         return 5
